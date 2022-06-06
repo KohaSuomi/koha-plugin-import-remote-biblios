49c7b4a9fac06ad92dfcb659e4ca7e47d0e2ee5d
Johanna Raisa <johanna.raisa@gmail.com>
2022-06-06 10:57:23 +0300

    Remove old symlink

--- set-development.sh
+++ set-development.sh
@@ -11,6 +11,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/ImportRemoteBiblios" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ImportRemoteBiblios
 ln -s "$SCRIPT_DIR/Koha/Plugin/Fi/KohaSuomi/ImportRemoteBiblios.pm" $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ImportRemoteBiblios.pm
 
+rm $kohadir/misc/cronjobs/run_import_remote_biblios.pl
 ln -s $kohaplugindir/Koha/Plugin/Fi/KohaSuomi/ImportRemoteBiblios/cronjobs/run_import_remote_biblios.pl $kohadir/misc/cronjobs/run_import_remote_biblios.pl
 
 perl $kohadir/misc/devel/install_plugins.pl
